d6893e6fab02ed3dd492e869324a5a2433e69db3
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-22 15:02:57 +0300

    feat: expose public Python API

---
+++ src/test_matrix/__init__.py
@@ -0,0 +1,8 @@
+"""Test Matrix: deterministic CI matrix generation."""
+
+from .core import MatrixError, MatrixResult, generate_matrix, validate_coverage
+
+__version__ = "1.0.0"
+__author__ = "Radwan Abdulhadi Ahmed / @rad03i2"
+
+__all__ = ["MatrixError", "MatrixResult", "generate_matrix", "validate_coverage"]
